1c096b5ebe725f2a5fa1406255b93c58cbe43b68
Byounghee Chae <mybeang@naver.com>
2026-05-20 15:56:25 +0900

    feat: 유용한 Utility 추가
    - Layer 비교 함수
    - JsonString -> JObject 변환

---
+++ Assets/Scripts/Common/Utils.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Utils
+{
+    public static class Layer
+    {
+        public static bool Compare(LayerMask myLayerMask, int targetLayer)
+        {
+            return ((1 << myLayerMask) & targetLayer) != 0;
+        }
+    }
+    
+    public static class JsonHandler
+    {
+        public static JObject ResourceFileLoader(string fileName)
+        {
+            TextAsset textAsset = Resources.Load<TextAsset>(fileName);
+            if (textAsset == null)
+            {
+                Debug.LogError($"Not found JSON file: Resources/{fileName}");
+                return null;
+            }
+            return JObject.Parse(textAsset.text);
+        }
+
+        public static JObject LoadFromString(string jsonString) => JObject.Parse(jsonString);
+    }
+}
