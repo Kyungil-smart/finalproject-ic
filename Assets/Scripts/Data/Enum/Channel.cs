2ee0cdea493877710487deac31b58e4854ef1cbd
Byounghee Chae <mybeang@naver.com>
2026-05-20 15:33:14 +0900

    feat: Data Dispatcher 구현

---
+++ Assets/Scripts/Data/Enum/Channel.cs
@@ -0,0 +1,7 @@
+﻿namespace DataDispatcher
+{
+    public enum Channel
+    {
+        // 채널명 기입
+    }    
+}
---
+++ Assets/Scripts/Interfaces/IPostManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DataDispatcher
+{
+    public interface IPostManager
+    {
+        // Channel 등록
+        public void Subscribe<T>(Channel key, Action<T> callback);
+        public void Subscribe<TReq, TRes>(Channel key, Func<TReq, TRes> callback);
+        // Channel 등록해제
+        public void Unsubscribe<T>(Channel key, Action<T> callback);
+        public void Unsubscribe<TReq, TRes>(Channel key, Func<TReq, TRes> callback);
+        // 데이터 전송
+        public void Post<T>(Channel key, T data);
+        public TRes Request<TReq, TRes>(Channel key, TReq data);
+    }    
+}
