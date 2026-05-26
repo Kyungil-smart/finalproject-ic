9a238e169ee2bde2062ab947e224100e45294991
Byounghee Chae <mybeang@naver.com>
2026-05-26 17:51:43 +0900

    feat: Event Type 및 Rendering 을 위한 데이터 Param 정의

---
+++ Assets/Scripts/UI/EventParams.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EventParams { }
+
+public class RegularEvent : EventParams
+{
+    public string mainText;
+    public List<(int id, string text)> choices = new();
+}
+
+public class Staff : RegularEvent { }
+public class Linkage : RegularEvent { }
+
+public class RewardEvent : EventParams
+{
+    public string mainText;
+    public Sprite gradeSprite;
+    public List<(int id, Sprite icon)> options = new();
+}
