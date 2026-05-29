2a494b797d4749448e12e9d82305c24a9660877d
156Sheong <waladeus@gmail.com>
2026-05-29 10:31:20 +0900

    fix: 12-4 프로세스 상태 끝나고 0.5초 쉬도록 수정
    - 한 번에 로그가 많이 뜨면 메모리 누수가 생겨 수정

--- Assets/Scripts/ProcessState/SubState/ReleaseAwardsSubState.cs
+++ Assets/Scripts/ProcessState/SubState/ReleaseAwardsSubState.cs
@@ -1,14 +1,9 @@
 using System;
 using UnityEngine;
+using System.Collections;
+
 
 public class ReleaseAwardsSubState : MonoBehaviour, IProcessState
-
-
-
-
-
-
-
 {
     [Header("이 상태의 SO")]
     [field: SerializeField] public ProcessStateSO StateData { get; private set; }   // 현재 상태
@@ -31,12 +26,25 @@ public class ReleaseAwardsSubState : MonoBehaviour, IProcessState
     {
         Debug.Log("[ReleaseReviewGamersSubState] : 12-4 어워즈 판정 확인 진행");
 
-        Exit();
+        StartCoroutine(Wait1SecondRoutine());
+
+        // Exit();
     }
 
     public void Exit()
     {
         IsFinished = true;
         OnStateFinished?.Invoke(this);
+    }
+
+
+    // 메모리 누수 확인 용 지연 코루틴 (한 번에 모든 순환이 작동하면 판단하기 힘들어 추가)
+    IEnumerator Wait1SecondRoutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        Debug.Log("0.5초 경과");
+
+        Exit();
     }
 }
