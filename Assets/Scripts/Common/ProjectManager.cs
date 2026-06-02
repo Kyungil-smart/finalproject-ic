1f42b7516a63cae3d5443129243fa50db59abd8a
Byounghee Chae <mybeang@naver.com>
2026-06-02 10:58:51 +0900

    feat: 1차 Project manager 데이터 정의 및 Interface 개발

---
+++ Assets/Scripts/Common/ProjectManager.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using R3;
+using UnityEngine;
+
+[Serializable]
+public struct NameTag
+{
+    public int id;
+    public string name;
+    public int textId;
+}
+
+[Serializable]
+public struct Quality
+{
+    public float total;
+    public float design;
+    public float development;
+    public float art;
+}
+
+[Serializable]
+public struct ProjectData
+{
+    public ReactiveProperty<Quality> Qualities;  // 프로젝트 퀄리티
+    public ReactiveProperty<bool> IsCompleted { get; }  // 프로젝트 완료 여부
+    public string name;  // 프로젝트 이름
+    public NameTag genre;  // 장르
+    public NameTag theme;  // 테마
+    public ProjectGrade grade;  // 등급 F ~ SSSS
+    public uint cost;  // 투자된 금액
+    public uint income;  // 매출 금액
+    public NameTag award;  // 수상 경력
+
+    public ProjectData(string name)
+    {
+        this.name = name;
+        genre = default;
+        theme = default;
+        grade = ProjectGrade.F;
+        cost = 0;
+        income = 0;
+        award = default;
+        Qualities = new();
+        IsCompleted = new();
+    }
+}
+
+public struct ProjectDataForUI
+{
+    public float totalQuality;
+    public float devQuality;
+    public float artQuality;
+    public float designQuality;
+    public bool isCompleted;
+    public string name;
+    public int genreTextId;
+    public int themeTextId;
+    public string gradeText;
+    public uint cost;
+    public uint income;
+    public int awardTextId;
+}
+
+public class ProjectManager : Manager, IProjectManager
+{
+    private ProjectData _projectData;
+
+    private void OnEnable() => Register();
+    private void OnDisable() => Unregister();
+
+    public float TotalQuality
+    {
+        get => _projectData.Qualities.Value.total;
+    }
+    
+    public float DevQuality
+    {
+        get => _projectData.Qualities.Value.development;
+        set
+        {
+            var data = _projectData.Qualities.Value;
+            data.development = value;
+            _projectData.Qualities.Value = data;
+        }
+    } 
+    
+    public float ArtQuality
+    {
+        get => _projectData.Qualities.Value.art;
+        set
+        {
+            var data = _projectData.Qualities.Value;
+            data.art = value;
+            _projectData.Qualities.Value = data;
+        }
+    } 
+    
+    public float DesignQuality
+    {
+        get => _projectData.Qualities.Value.design;
+        set
+        {
+            var data = _projectData.Qualities.Value;
+            data.design = value;
+            _projectData.Qualities.Value = data;
+        }
+    } 
+    
+    public NameTag Genre
+    {
+        get => _projectData.genre; 
+        set => _projectData.genre = value;   
+    }
+
+    public NameTag Theme
+    {
+        get => _projectData.theme; 
+        set => _projectData.theme = value;
+    }
+
+    public uint Cost
+    {
+        get => _projectData.cost;
+        set => _projectData.cost = value;
+    }
+
+    public uint Income
+    {
+        get => _projectData.income;
+        set => _projectData.income = value;
+    }
+    
+    public void NewProject(string projectName)
+    {
+        _projectData = new ProjectData(projectName);
+    }
+
+    public void LoadProject(string jsonData)
+    {
+        // ToDo. 현재 진행중인 프로젝트의 JSON Data Load
+        _projectData = JsonUtility.FromJson<ProjectData>(jsonData);
+    }
+
+    public string ToJsonData()
+    {
+        return JsonUtility.ToJson(_projectData);
+    }
+
+    public ProjectData FinishProject()
+    {
+        _projectData.IsCompleted.Value = true;
+        return _projectData;
+    }
+
+    public void CalculateGrade()
+    {
+        // ToDo. 등급 받는 계산 식 넣기
+    }
+
+    public void JudgingAward()
+    {
+        // ToDo. 수상 받는 계산 식 넣기 (애매하네 이건... 외부에서 주입하는게 좋을까?)
+    }
+
+    public ProjectData GetProjectData() => _projectData;
+
+    public ProjectDataForUI GetProjectDataForUI()
+    {
+        var data = new ProjectDataForUI();
+        data.totalQuality = _projectData.Qualities.Value.total;
+        data.devQuality = _projectData.Qualities.Value.development;
+        data.artQuality = _projectData.Qualities.Value.art;
+        data.designQuality = _projectData.Qualities.Value.design;
+        data.isCompleted = _projectData.IsCompleted.Value;
+        data.name = _projectData.name;
+        data.genreTextId = _projectData.genre.textId;
+        data.themeTextId = _projectData.theme.textId;
+        data.cost = _projectData.cost;
+        data.income = _projectData.income;
+        data.awardTextId = _projectData.award.textId;
+        return data;
+    }
+
+    protected override void Register() => ServiceLocater.Register<IProjectManager>(this);
+    protected override void Unregister()=> ServiceLocater.Unregister<IProjectManager>(this);
+}
