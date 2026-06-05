c2d7ceed1304b35e5fa98ffae935862554711b0a
Byounghee Chae <mybeang@naver.com>
2026-06-05 12:41:24 +0900

    feat: ready staff

--- Assets/Scripts/Bootstrap/FirstSettings.cs
+++ Assets/Scripts/Bootstrap/FirstSettings.cs
@@ -5,5 +5,11 @@ using UnityEngine;
 /// </summary>
 public class FirstSettings : MonoBehaviour
 {
-    
+    private void Initializing()
+    {
+        // 초기 직원 수 2, level 1
+        // 직원 확인
+        // 확인한 직원으로 채용 진행
+        // 끝난 후 끝났다는 것을 표기하기
+    }
 }
