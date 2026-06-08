65d320772a74319bce3df38ed78336838c9847df
Crowasang <bighyun620@naver.com>
2026-06-08 11:08:56 +0900

    feat: 직원간 이벤트 실행 모듈 개발

--- Assets/Scripts/Event/StaffEventTaskRunner.cs
+++ Assets/Scripts/Event/StaffEventTaskRunner.cs
@@ -10,8 +10,33 @@ public class StaffEventTaskRunner : IEventTaskRunner
 
     public async UniTask Execute()
     {
-        throw new System.NotImplementedException();
+        Debug.Log("[StaffEventTaskRunner] Start to run");
+        
+        Debug.Log($"[StaffEventTaskRunner] main: ID:{_taskData.id} MainText:{_taskData.titleTextId} Desc:{_taskData.descTextId}");
+        foreach (var btn in _taskData.buttons)
+            Debug.Log($"[StaffEventTaskRunner] btns - Target:{btn.target} Value:{btn.effectValue} Ratio:{btn.effectRatio}");
+        
+        var tcs = new UniTaskCompletionSource<int>();
+        
+        var renderData = new NormalEventUIRenderData(
+            eventType: EventType.Regular,
+            mainTextId: _taskData.titleTextId,
+            callback: (selectedId) => tcs.TrySetResult(selectedId)
+        );
+        
+        for (int i = 0; i < _taskData.buttons.Count; i++)
+        {
+            var btn = _taskData.buttons[i];
+            if (btn.textId != 0) renderData.choices.Add((i, btn.textId));
+        }
+        
+        ServiceLocater.Get<IUIRouter>().NavigateTo(UIType.EventUI, renderData);
+        
+        var selectedIndex = await tcs.Task;
+        // Apply(_taskData.buttons[selectedIndex]);
     }
+    
+    // Todo. 결과 반영은 아직 모름
 }
 
 public enum Synergy {Good, Normal, Bad}
