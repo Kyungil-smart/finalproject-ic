6180f0b19532b9e032d7f5d8b8244e95c9e8212a
Crowasang <bighyun620@naver.com>
2026-06-09 12:06:43 +0900

    feat: 유효스탯 계산기능 추가

--- Assets/Scripts/Common/QualityCalculate.cs
+++ Assets/Scripts/Common/QualityCalculate.cs
@@ -2,5 +2,38 @@ using UnityEngine;
 
 public class QualityCalculate
 {
+    private QualityDataSO _qualityData;
+    
+    private float _designQuality;
+    private float _devQuality;
+    private float _artQuality;
+    private float _totalQuality;
+    private float _achieve;
+
+    public QualityCalculate(QualityDataSO qualityData)
+    {
+        _qualityData = qualityData;
+    }
+    
+    // 유효스탯 계산
+    private float CalCuateArrange(StaffEntity main, StaffEntity sub)
+    {
+        var data = _qualityData.rates[0];
+        float mainStat = main.GetJob() switch
+        {
+            JobType.Artist => main.GetArt(),
+            JobType.Developer => main.GetDevelopment(),
+            JobType.Planner => main.GetPlanning()
+        };
+        float subStat = sub.GetJob() switch
+        {
+            JobType.Artist => sub.GetArt(),
+            JobType.Developer => sub.GetDevelopment(),
+            JobType.Planner => sub.GetPlanning()
+        };
+        return (mainStat * data.arrageCase1) + (subStat * data.arrageCase2);
+    }
+    
+    // 파트별 퀄리티
     
 }
