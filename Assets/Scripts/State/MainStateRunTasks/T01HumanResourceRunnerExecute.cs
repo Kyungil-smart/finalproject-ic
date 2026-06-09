25d72ca0b779752458493ba19dba7deb8dbda2eb
156Sheong <waladeus@gmail.com>
2026-06-09 10:46:56 +0900

    feat: 1단계 채용 진행 프로세스 작업

--- Assets/Scripts/State/MainStateRunTasks/T01HumanResourceRunnerExecute.cs
+++ Assets/Scripts/State/MainStateRunTasks/T01HumanResourceRunnerExecute.cs
@@ -4,6 +4,7 @@ using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 /// <summary>
@@ -12,17 +13,24 @@ using UnityEngine;
 public class T01HumanResourceRunnerExecute : ProcessTaskRunner, IProcessTaskRunnerExecute
 {
     private List<StaffViewData> _candidateStaffList;
+    private List<StaffViewData> _tempHiredList;
+    private StaffSummaryRenderData _staffSummaryRenderData;
+
+    private List<int> _selectedStaffs;
 
 
 
     // 후보 리스트 생성 및 변환 -> 직원 리스트 확인 -> 기존 직원과 리스트 통합 -> 계약할 캐릭터 선택 -> 계약 및 채용 확정 -> 채용 진행 애니메이션 -> 채용 확인
     public async UniTask Execute()
     {
-        await CreateCandidateList();
-        CheckStaffList();
+        _staffSummaryRenderData = new StaffSummaryRenderData();
+        _selectedStaffs = new List<int>();
+
+
         MergeStaffList();
-        List<int> number = new List<int> { 0, 2, 3 };    // !! TODO: 임시용, 테스트 완료 후 삭제 요망
-        HireStaff(number);
+        // List<int> number = new List<int> { 0, 2, 3 };    // !! TODO: 임시용, 테스트 완료 후 삭제 요망
+        //HireStaff(number);
+
         WaitingAnimation();
         CheckHiring();
     }
@@ -45,18 +53,15 @@ public class T01HumanResourceRunnerExecute : ProcessTaskRunner, IProcessTaskRunn
 
 
         // 아래는 필요한 데이터 위치 확인 + ServiceLocater / R3 / UniTask 기능 작동하는지 보기 위해서 예시로 넣음
-
-        // 슬롯 개수
-        int _totalSlotCount = 8;    // 전체 슬롯 개수(TODO : 어디서 불러오는지 확인 필요)   
-        int _hiredSlotCount = ServiceLocater.Get<IStaffRegister>().GetAllHiredStaffList().Count;    // 이미 고용된 슬롯 개수        
+     
 
         // 필요한 인터페이스 불러오기
         var gameManager = ServiceLocater.Get<IGameManager>();
         var staffManager = ServiceLocater.Get<IStaffRecruit>();
 
-        await staffManager.GenerateRecruitCandidatesAsync(playerLevel: gameManager.PlayerLevel.CurrentValue, cardCount: _totalSlotCount - _hiredSlotCount);
+        await staffManager.GenerateRecruitCandidatesAsync(playerLevel: gameManager.PlayerLevel.CurrentValue, cardCount: 8);
 
-        Debug.Log($"회사 레벨: {gameManager.PlayerLevel.CurrentValue} | 회사 슬롯: {_totalSlotCount - _hiredSlotCount}");
+        // Debug.Log($"회사 레벨: {gameManager.PlayerLevel.CurrentValue} | 회사 슬롯: {_totalSlotCount - _hiredSlotCount}");
 
         return staffManager.GetAvailableStaffList();
     }
@@ -95,14 +100,55 @@ public class T01HumanResourceRunnerExecute : ProcessTaskRunner, IProcessTaskRunn
         List<StaffViewData> staffList = CheckStaffList();
 
         _candidateStaffList = new List<StaffViewData>();
-        _candidateStaffList.AddRange(candidateList);
         _candidateStaffList.AddRange(staffList);
+        _candidateStaffList.AddRange(candidateList);
+
+        // var data = new SimpleUIRenderData(so.stateNameId, 9900007, GoProcess);  // TODO: 확인 필요
+
+
+        _staffSummaryRenderData.staffSummaryData = new List<StaffSummaryData>();
+
+        if (_staffSummaryRenderData == null)
+        {
+            foreach (var item in staffList)
+            {
+                StaffSummaryData staffSummaryData = new StaffSummaryData();
+                staffSummaryData.selected = false;
+                staffSummaryData.hired = true;
+                staffSummaryData.viewData = item;
+
+                _staffSummaryRenderData.staffSummaryData.Add(staffSummaryData);
+            }
+
+            foreach (var item in candidateList)
+            {
+                StaffSummaryData staffSummaryData = new StaffSummaryData();
+                staffSummaryData.selected = false;
+                staffSummaryData.hired = false;
+                staffSummaryData.viewData = item;
+
+                _staffSummaryRenderData.staffSummaryData.Add(staffSummaryData);
+            }
+        }
+
+        _staffSummaryRenderData.callbacks = SelectedStaffCallback;
+
+
+        ServiceLocater.Get<IUIRouter>().NavigateTo(UIType.StaffCandidateUI, _staffSummaryRenderData);
 
     }
 
+    private List<int> SelectedStaffCallback(List<int> staffs)
+    {
+        _selectedStaffs = staffs;
+        return staffs;
+    }
+
+
+    // 가채용
     public void HireStaff(List<int> indexNumber)
     {
-        // TODO : 직원 관리 4: 계약 및 채용 확정(실제 확정은 아니고 문서 명칭대로 함)
+        // TODO : 직원 관리 4: 계약 및 채용(실제 확정은 아니고 가채용, 문서 명칭대로 함)
         // 필요 Input 데이터
         //   UI에서 선택 받은 인덱스 넘버
 
@@ -112,6 +158,17 @@ public class T01HumanResourceRunnerExecute : ProcessTaskRunner, IProcessTaskRunn
         // 필요 Output 데이터
         //   리스트업 해서 던지기
         //   UI에도 전달
+
+        _tempHiredList = new List<StaffViewData>();
+
+        // 순회하며 해당 인덱스의 데이터를 _tempHiredList에 추가
+        foreach (int idx in indexNumber)
+        {
+            if (idx >= 0 && idx < _candidateStaffList.Count)
+            {
+                _tempHiredList.Add(_candidateStaffList[idx]);
+            }
+        }
     }
 
     private void WaitingAnimation()
@@ -123,10 +180,12 @@ public class T01HumanResourceRunnerExecute : ProcessTaskRunner, IProcessTaskRunn
         // 필요 기능
         //   채용 진행
         //   해고 진행
-        //   채용 진행 애니메이션 실행 및 대기
+        //   채용 진행 애니메이션 실행 및 대기 -> 어떻게 하지?? 애니메이션 실제 진행은 에셋 나온 후에 TODO
 
 
         // 필요 Output 데이터
+
+
     }
 
     private void CheckHiring()
@@ -139,6 +198,8 @@ public class T01HumanResourceRunnerExecute : ProcessTaskRunner, IProcessTaskRunn
 
         // 필요 Output 데이터
         //   최신화된 GetAllHiredStaffList() 전달하기
-    }
 
+
+
+    }
 }
