34a175952855c15dae5426a8645de59bc3c774bd
Byounghee Chae <mybeang@naver.com>
2026-06-09 17:09:57 +0900

    feat: T02-T03 Rendering 용 데이터 정의

---
+++ Assets/Scripts/UI/Process/DataClasses/T03TrendGenreThemeRenderData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T03TrendGenreThemeSelectRenderData : UIRenderData
+{
+    public List<NameTag> genres;
+    public List<NameTag> themes;
+    public Action<(NameTag genre, NameTag theme)> onSelectCallback ;
+}
+
+public class T03TrendGenreThemeResultRenderData : UIRenderData
+{
+    public NameTag genre;
+    public NameTag theme;
+    public Action goBackCallback;
+    public Action goNextCallback;
+}
