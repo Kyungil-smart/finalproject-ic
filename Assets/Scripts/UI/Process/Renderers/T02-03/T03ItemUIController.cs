11b98a2d514cf7aaa2b50b6f5cbcb82a5e9cfcfd
Byounghee Chae <mybeang@naver.com>
2026-06-09 19:26:55 +0900

    feat: T02-03 기본 틀 만들기

---
+++ Assets/Scripts/UI/Process/Renderers/T02-03/T03ItemUIController.cs
@@ -0,0 +1,9 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class T03ItemUIController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI itemNameText;
+    
+    public void SetString(string itemName) => itemNameText.text = itemName;
+}
---
+++ Assets/Scripts/UI/Process/Renderers/T02-03/T03UIController.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class T03UIController : MonoBehaviour
+{
+    
+}
