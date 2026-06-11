b913f788774a56c840e5cac4fd4856483fedd22c
Byounghee Chae <mybeang@naver.com>
2026-06-11 12:07:52 +0900

    feat: T3 관련 UI 추가

--- Assets/Scripts/UI/Process/Renderers/T02-03/T03ItemUIController.cs
+++
@@ -1,9 +0,0 @@
-﻿using TMPro;
-using UnityEngine;
-
-public class T03ItemUIController : MonoBehaviour
-{
-    [SerializeField] private TextMeshProUGUI itemNameText;
-    
-    public void SetString(string itemName) => itemNameText.text = itemName;
-}
