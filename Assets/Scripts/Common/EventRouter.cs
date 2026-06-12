96535d25b13f3330df1b32e7c673ed4b984fce01
Crowasang <bighyun620@naver.com>
2026-06-12 10:02:46 +0900

    feat: 골드리워드 기능추가

--- Assets/Scripts/Common/EventRouter.cs
+++ Assets/Scripts/Common/EventRouter.cs
@@ -5,12 +5,13 @@ public class EventRouter : IEventRouter
 {
     private Dictionary<string, IEventRouter> _router = new()
     {
-        {
-            "Total_Quality", new QualityReward()
-        }
+        {"Total_Quality", new QualityReward()},
+        {"Money", new GoldReward()}
     };
     public void Apply(EventButtonData btn)
     {
-        
+        if (string.IsNullOrEmpty(btn.target)) return;
+        if (_router.TryGetValue(btn.target, out var target)) target.Apply(btn);
+        else Debug.LogWarning("Target not found: " + btn.target);
     }
 }
--- Assets/Scripts/Common/RewardApply.cs
+++ Assets/Scripts/Common/RewardApply.cs
@@ -14,3 +14,12 @@ public class QualityReward : IEventRouter
         Debug.Log(qt.TotalQuality);
     }
 }
+
+public class GoldReward : IEventRouter
+{
+    public void Apply(EventButtonData btn)
+    {
+        ServiceLocater.Get<IGameManager>().AddMoney(btn.effectValue);
+        Debug.Log(ServiceLocater.Get<IGameManager>().Money);
+    }
+}
