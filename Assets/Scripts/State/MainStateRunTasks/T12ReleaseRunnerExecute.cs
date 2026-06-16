e887a67e757463c3604795c4a1518f399e32d501
156Sheong <waladeus@gmail.com>
2026-06-16 15:51:21 +0900

    feat: T12 프로세스 상태 기능 추가중

--- Assets/Scripts/State/MainStateRunTasks/T12ReleaseRunnerExecute.cs
+++ Assets/Scripts/State/MainStateRunTasks/T12ReleaseRunnerExecute.cs
@@ -1,42 +1,116 @@
 using Cysharp.Threading.Tasks;
 using System;
+using System.Diagnostics;
 using UnityEngine;
 
+
+/// <summary>
+/// 출시(T12)
+/// </summary>
 public class T12ReleaseRunnerExecute : ProcessTaskRunner, IProcessTaskRunnerExecute
 {
+    private bool _endProcess;
+    private bool _conditionGoback;
+
+    // (유저 / 평론가 리뷰 생략) -> 매출 집계 애니메이션 -> 매출 결과 확인 -> 어워즈 심사 애니메이션 -> 어워즈 결과 확인 -> 프로젝트 총 결산 -> 프로젝트 종료
     public async UniTask Execute()
     {
-        ReviewGamers();
-        ReviewCritics();
-        WorkOnProcess();
-        CalculateRevenue();
-        NominatedAwards();
+        _endProcess = false;
+        await CalculateRevenue();
+        await UniTask.WaitUntil(() => _endProcess);
     }
 
-    private void ReviewGamers()
+
+    // 매출 발생 및 매출 집계 애니메이션 완료 대기
+    private async UniTask CalculateRevenue()
     {
-        Debug.Log($"[ReleaseReviewGamersSubState] : 게이머 리뷰 진행");
-        // TODO : 유저 반응 확인
+        _waiting = true;
+        // ToDO. Animation 이 들어올 경우 대비 해야함.
+        var data = new SimpleUIRenderData(9900020, 9900007, GoProcess);
+        ServiceLocater.Get<IUIRouter>().NavigateTo(UIType.ProcessSimpleUI, data);
+
+        // TODO: 매출 발생 기능 추가 필요
+
+        await WaitProcess();
+        await CheckRevenue();
     }
 
-    private void ReviewCritics()
+    private void GoProcess()
     {
-        Debug.Log($"[ReleaseReviewGamersSubState] : 평론가 리뷰 진행");
-        // TODO : 평론가 반응 확인
+        _waiting = false;
     }
 
-    private void WorkOnProcess()
+    // 매출 결과 확인
+    private async UniTask CheckRevenue()
     {
-        // TODO : 작업 진행 페이즈
+        _waiting = true;
+
+
+        await WaitProcess();
+        await CalculateAwards();
     }
 
-    private void CalculateRevenue()
+
+    // 어워즈 및 어워즈 애니메이션 완료 대기
+    private async UniTask CalculateAwards()
     {
-        // TODO : 매출 발생
+        _waiting = true;
+        // ToDO. Animation 이 들어올 경우 대비 해야함.
+        var data = new SimpleUIRenderData(9900020, 9900007, GoProcessB);
+        ServiceLocater.Get<IUIRouter>().NavigateTo(UIType.ProcessSimpleUI, data);
+
+        // TODO: 어워즈 기능 추가 필요
+
+        await WaitProcess();
+        await CheckAwards();
     }
 
-    private void NominatedAwards()
+    private void GoProcessB()
     {
-        // TODO : 어워즈 선정
+        _waiting = false;
     }
+
+    // 어워즈 결과 확인
+    private async UniTask CheckAwards()
+    {
+        _waiting = true;
+
+        // TODO : 어워즈 결과 UI 추가되면 넣어야 함
+
+        await WaitProcess();
+        await CheckTotal();
+    }
+
+    // 프로젝트 총 결산 확인
+    private async UniTask CheckTotal()
+    {
+        _waiting = true;
+
+        // TODO : 총결산 UI 추가되면 넣어야 함
+
+        await WaitProcess();
+        await EndProject();
+    }
+
+    // 프로젝트 종료
+    private async UniTask EndProject()
+    {
+        _waiting = true;
+
+
+
+        // ServiceLocater.Get<IStaffRegister>().GetExpAllStaffs() TODO : 릴리즈 때 전체 스텝에게 경험치 주기
+
+
+        await UniTask.Yield();
+        await GoToNextProcess();
+        await WaitProcess();
+    }
+
+    private async UniTask GoToNextProcess()
+    {
+        _waiting = false;
+        _endProcess = true;
+    }
+
 }
