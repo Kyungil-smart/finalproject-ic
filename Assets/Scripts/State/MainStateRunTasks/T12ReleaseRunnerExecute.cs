3f107e6ce543ad0fcbb4cee9a551c8cb7d21dfa3
156Sheong <waladeus@gmail.com>
2026-06-17 06:04:10 +0900

    feat: T12 한바퀴 돌아가도록 수정

--- Assets/Scripts/State/MainStateRunTasks/T12ReleaseRunnerExecute.cs
+++ Assets/Scripts/State/MainStateRunTasks/T12ReleaseRunnerExecute.cs
@@ -20,24 +20,16 @@ public class T12ReleaseRunnerExecute : ProcessTaskRunner, IProcessTaskRunnerExec
         await UniTask.WaitUntil(() => _endProcess);
     }
 
-
-    // 매출 발생 및 매출 집계 애니메이션 완료 대기
+    // 매출 받아오기 및 매출 집계 애니메이션 완료 대기
     private async UniTask CalculateRevenue()
     {
         _waiting = true;
+
+        // TODO: 매출 건내주는 기능이 필요할 수도 있음
         // ToDO. Animation 이 들어올 경우 대비 해야함.
-        var data = new SimpleUIRenderData(9900020, 9900007, GoProcess);
-        ServiceLocater.Get<IUIRouter>().NavigateTo(UIType.ProcessSimpleUI, data);
 
-        // TODO: 매출 발생 기능 추가 필요
-
-        await WaitProcess();
+        // await WaitProcess();
         await CheckRevenue();
-    }
-
-    private void GoProcess()
-    {
-        _waiting = false;
     }
 
     // 매출 결과 확인
@@ -45,24 +37,45 @@ public class T12ReleaseRunnerExecute : ProcessTaskRunner, IProcessTaskRunnerExec
     {
         _waiting = true;
 
+        T12IncomeUIRenderData t12IncomeUIRenderData = new T12IncomeUIRenderData();
+        t12IncomeUIRenderData.btCallback = GoProcessA;
+
+        ServiceLocater.Get<IUIRouter>().NavigateTo(UIType.ReleaseUI, t12IncomeUIRenderData);
 
         await WaitProcess();
         await CalculateAwards();
     }
 
+    private void GoProcessA()
+    {
+        _waiting = false;
+    }
 
     // 어워즈 및 어워즈 애니메이션 완료 대기
     private async UniTask CalculateAwards()
     {
         _waiting = true;
-        // ToDO. Animation 이 들어올 경우 대비 해야함.
-        var data = new SimpleUIRenderData(9900020, 9900007, GoProcessB);
-        ServiceLocater.Get<IUIRouter>().NavigateTo(UIType.ProcessSimpleUI, data);
 
-        // TODO: 어워즈 기능 추가 필요
+        // ServiceLocater.Get<IProjectManager>().JudgingAward();    // TODO: 어워즈 기능 추가되면 수정 필요
+        // ToDO. Animation 이 들어올 경우 대비 해야함.
+
+        // await WaitProcess();
+        await CheckAwards();
+    }
+
+
+    // 어워즈 결과 확인
+    private async UniTask CheckAwards()
+    {
+        _waiting = true;
+
+        T12AwardsUIRenderData t12AwardsUIRenderData = new T12AwardsUIRenderData();
+        t12AwardsUIRenderData.btCallback = GoProcessB;
+
+        ServiceLocater.Get<IUIRouter>().NavigateTo(UIType.ReleaseUI, t12AwardsUIRenderData);
 
         await WaitProcess();
-        await CheckAwards();
+        await CheckTotal();
     }
 
     private void GoProcessB()
@@ -70,26 +83,25 @@ public class T12ReleaseRunnerExecute : ProcessTaskRunner, IProcessTaskRunnerExec
         _waiting = false;
     }
 
-    // 어워즈 결과 확인
-    private async UniTask CheckAwards()
-    {
-        _waiting = true;
-
-        // TODO : 어워즈 결과 UI 추가되면 넣어야 함
-
-        await WaitProcess();
-        await CheckTotal();
-    }
-
     // 프로젝트 총 결산 확인
     private async UniTask CheckTotal()
     {
         _waiting = true;
 
-        // TODO : 총결산 UI 추가되면 넣어야 함
+        // TODO : 장르테마 & 등급 & 투자비 & 매출 & 시상기록 받아오기
+
+        T12ProjectDetailUIRenderData t12ProjectDetailUIRenderData = new T12ProjectDetailUIRenderData();
+        t12ProjectDetailUIRenderData.btCallback = GoProcessC;
+
+        ServiceLocater.Get<IUIRouter>().NavigateTo(UIType.ReleaseUI, t12ProjectDetailUIRenderData);
 
         await WaitProcess();
         await EndProject();
+    }
+
+    private void GoProcessC()
+    {
+        _waiting = false;
     }
 
     // 프로젝트 종료
@@ -98,9 +110,8 @@ public class T12ReleaseRunnerExecute : ProcessTaskRunner, IProcessTaskRunnerExec
         _waiting = true;
 
 
-
-        // ServiceLocater.Get<IStaffRegister>().GetExpAllStaffs() TODO : 릴리즈 때 전체 스텝에게 경험치 주기
-
+        // 전체 스텝에게 경험치 주기
+        ServiceLocater.Get<IStaffRegister>().GetExpAllStaffs();
 
         await UniTask.Yield();
         await GoToNextProcess();
