38b7794ca19a84b43ff84874fdacc26cf1ff3425
156Sheong <waladeus@gmail.com>
2026-06-22 16:27:18 +0900

    feat: T12 애니메이션 가기능 추가 + 디버그 로그 간략화

--- Assets/Scripts/State/MainStateRunTasks/T12ReleaseRunnerExecute.cs
+++ Assets/Scripts/State/MainStateRunTasks/T12ReleaseRunnerExecute.cs
@@ -26,13 +26,27 @@ public class T12ReleaseRunnerExecute : ProcessTaskRunner, IProcessTaskRunnerExec
     private async UniTask CalculateRevenue()
     {
         _waiting = true;
+        // ToDO. Animation 추가 작업 필요.
 
-        // TODO: 매출 건내주는 기능이 필요할 수도 있음
-        // ToDO. Animation 이 들어올 경우 대비 해야함.
+        var data = new ProgressAnimationRenderData()
+        {
+            staticImage = null,
+            progressTexts = new() { "플랫폼 수수료 제외 수익 집계", "광고 효율 산출", "프로젝트 비용 및 임금 계산", "공유용 최종 매출 리포트 작성" },
+            callback = GoProcessD,
+        };
+        ServiceLocater.Get<IUIRouter>().NavigateTo(UIType.ProcAnimationUI, data);
 
-        // await WaitProcess();
+        // TODO: 매출 건내주는 기능이 나오면 수정이 필요할 수도 있음
+
+        await WaitProcess();
         await CheckRevenue();
     }
+
+    private void GoProcessD()
+    {
+        _waiting = false;
+    }
+
 
     // 매출 결과 확인
     private async UniTask CheckRevenue()
@@ -58,20 +72,12 @@ public class T12ReleaseRunnerExecute : ProcessTaskRunner, IProcessTaskRunnerExec
     {
         _waiting = true;
 
-        Debug.Log($"[디버그] ServiceLocater 체크: {ServiceLocater.Get<IQualityManager>() != null}");
-        Debug.Log($"[디버그] Calculator 체크: {ServiceLocater.Get<IQualityManager>()?.Calculator != null}");
-
-
         // 어워즈 판단해서 프로젝트 매니저에 넣기
         curAwardsData.reqDesign = (int)ServiceLocater.Get<IQualityManager>().Calculator.GetDesignAchieve();
         curAwardsData.reqArt = (int)ServiceLocater.Get<IQualityManager>().Calculator.GetArtAchieve();
         curAwardsData.reqDev = (int)ServiceLocater.Get<IQualityManager>().Calculator.GetDevAchieve();
 
-
-        Debug.Log($"[디버그] AwardsDataSO 체크: {ServiceLocater.Get<IProjectDataManager>()?.AwardsDataSO != null}");
-        Debug.Log($"[디버그] AwardsDataSO 리스트 체크: {ServiceLocater.Get<IProjectDataManager>()?.AwardsDataSO.awardsDataList != null}");
-        Debug.Log($"[디버그] AwardsDataSO 리스트 숫자 체크: {ServiceLocater.Get<IProjectDataManager>()?.AwardsDataSO.awardsDataList.Count}");
-
+        Debug.Log($"현재 점수 : {curAwardsData.reqDesign} {curAwardsData.reqArt} {curAwardsData.reqDev}");
 
         // 기준이 되는 어워즈 SO(리스트) 불러오기
         List <AwardsData> awardsList  = ServiceLocater.Get<IProjectDataManager>().AwardsDataSO.awardsDataList;
@@ -93,20 +99,33 @@ public class T12ReleaseRunnerExecute : ProcessTaskRunner, IProcessTaskRunnerExec
 
         // 프로젝트 매니저에 수상 넣어주기
         ServiceLocater.Get<IProjectManager>().SetAwards(awardsList[index]);
-        Debug.Log($"어워즈 번호 : {awardsList[index]}");
-
 
         // 수상에 따른 금액 추가하기
         if(awardsList[index].target == "Money")
         {
             ServiceLocater.Get<IGameManager>().AddMoney(awardsList[index].value);
-            Debug.Log($"어워즈 보상 : {awardsList[index].target} | {awardsList[index].value}");
         }
 
-        // ToDO. Animation 이 들어올 경우 대비 해야함.
+        Debug.Log($"어워즈 번호 : {awardsList[index]} | 보상 {awardsList[index].target} | {awardsList[index].value}");
 
-        // await WaitProcess();
+        // ToDO. Animation 추가 작업 필요.
+
+        var data = new ProgressAnimationRenderData()
+        {
+            staticImage = null,
+            progressTexts = new() { "시상 후보 명단 등재", "비공개 기술 심사 진행", "유저 투표 점수 합산", "최종 수상작 발표" },
+            callback = GoProcessE,
+        };
+        ServiceLocater.Get<IUIRouter>().NavigateTo(UIType.ProcAnimationUI, data);
+
+
+        await WaitProcess();
         await CheckAwards();
+    }
+
+    private void GoProcessE()
+    {
+        _waiting = false;
     }
 
 
@@ -157,7 +176,9 @@ public class T12ReleaseRunnerExecute : ProcessTaskRunner, IProcessTaskRunnerExec
 
 
         // 전체 스텝에게 경험치 주기
+        Debug.Log($"0번 스태프 경험치 주기 전 : {ServiceLocater.Get<IStaffRegister>().GetAllHiredStaffList()[0].Staff_Name} | {ServiceLocater.Get<IStaffRegister>().GetAllHiredStaffList()[0].Exp}");
         ServiceLocater.Get<IStaffRegister>().GetExpAllStaffs();
+        Debug.Log($"0번 스태프 경험치 준 후 : {ServiceLocater.Get<IStaffRegister>().GetAllHiredStaffList()[0].Staff_Name} | {ServiceLocater.Get<IStaffRegister>().GetAllHiredStaffList()[0].Exp}");
 
         // 프로젝트에 추가하기
         ServiceLocater.Get<IGameManager>().AddProject(ServiceLocater.Get<IProjectManager>().GetProjectData());
