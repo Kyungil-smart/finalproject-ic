c77127ce13ab54ffc92c7064e0ecd8bf2823c431
Crowasang <bighyun620@naver.com>
2026-06-25 14:15:59 +0900

    feat: 마케팅 선택을 안할시 디폴트값 설정

--- Assets/Scripts/UI/Process/Renderers/T11/T11UIController.cs
+++ Assets/Scripts/UI/Process/Renderers/T11/T11UIController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using R3;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -22,13 +23,15 @@ public class T11UIController : MonoBehaviour
     [SerializeField] private Button selectBtn2;
 
     private List<int> _selectedMarketings = new();
+    private MarketingData _defaultMarketing;
+    public MarketingData defaultMarketing => _defaultMarketing; 
 
     private void Awake()
     {
         // Todo. 마케팅 UIType 성우님이 만드신다고 하셔서 놔둠
         // ServiceLocater.Get<IUIRouter>().RegisterUIRender();
-        // foreach (var panel in marketingPanels)
-        //     panel.OnItemSelect.Subscribe(SelectItem)
+        foreach (var panel in marketingPanels)
+            panel.OnItemSelect.Subscribe(SelectItem).AddTo(panel);
     }
 
     private void OnEnable()
@@ -49,33 +52,41 @@ public class T11UIController : MonoBehaviour
             _selectedMarketings.Clear();
             var cnt = 0;
 
-            for (int i = 0; i < renderData.marketingData.Count; i++)
+            for (int i = 0; i < marketingPanels.Count; i++)
             {
                 SetUpPanel(i, renderData.marketingData[i], marketingPanels[i], renderData.selectable);
                 marketingPanels[i].gameObject.SetActive(true);
                 if (renderData.marketingData[i].selected
-                    && _selectedMarketings.Contains(i))
+                    && !_selectedMarketings.Contains(i))
                 {
                     _selectedMarketings.Add(i);
                     cnt++;
                 }
             }
-
+            
+            _defaultMarketing = renderData.marketingData[marketingPanels.Count];
             switch (renderData.tailType.num)
             {
                 case 1:
                     tail1Panel.SetActive(true);
                     selectBtn.onClick.RemoveAllListeners();
                     selectBtn.onClick.AddListener(Close);
-                    selectBtn.onClick.AddListener(() => renderData.tailType.nextCallback());
+                    selectBtn.onClick.AddListener(() =>
+                    {
+                        if (_selectedMarketings.Count == 0)
+                        {
+                            // 디폴트값 처리
+                        }
+                        renderData.tailType.nextCallback();
+                    });
                     selectBtn.onClick.AddListener(() => mainPanel.SetActive(false));
                     break;
                 case 2:
                     tail2Panel.SetActive(true);
-                    selectBtn.onClick.RemoveAllListeners();
-                    selectBtn.onClick.AddListener(Close);
-                    selectBtn.onClick.AddListener(() => renderData.tailType.nextCallback());
-                    selectBtn.onClick.AddListener(() => mainPanel.SetActive(false));
+                    selectBtn2.onClick.RemoveAllListeners();
+                    selectBtn2.onClick.AddListener(Close);
+                    selectBtn2.onClick.AddListener(() => renderData.tailType.nextCallback());
+                    selectBtn2.onClick.AddListener(() => mainPanel.SetActive(false));
                     break;
             }
         }
