949a4d475d1286426cf1fb19c5a302631be379be
Crowasang <bighyun620@naver.com>
2026-06-30 10:57:48 +0900

    feat: 사운드매니저 추가 및 ui프리펩 생성

--- Assets/Scripts/Common/SoundManager.cs
+++ Assets/Scripts/Common/SoundManager.cs
@@ -3,37 +3,59 @@ using UnityEngine;
 
 public class SoundManager : Manager, ISoundManager
 {
-    protected override void Register() => ServiceLocater.Register(this);
+    [SerializeField] private AudioSource bgmSource;
+    [SerializeField] private AudioSource sfxSource;
+    
+    private float _bgmVolume = 1f;
+    private float _sfxVolume = 1f;
+    
+    private const string BGMKEY = "BGM";
+    private const string SFXKEY = "SFX";
+    
+    protected override void Register()
+    {
+        ServiceLocater.Register(this);
+        LoadVolume();
+    }
 
     protected override void Unregister() => ServiceLocater.Unregister(this);
+
+    public float GetSfxVolume() => _sfxVolume;
+    public float GetBgmVolume() => _bgmVolume;
+    
+    public void LoadVolume()
+    {
+        _sfxVolume = PlayerPrefs.GetFloat(SFXKEY, 0.5f);
+        _bgmVolume = PlayerPrefs.GetFloat(BGMKEY, 0.5f);
+        bgmSource.volume = _bgmVolume;
+        sfxSource.volume = _sfxVolume;
+    }
     
     public void SetSfxVolume(float volume)
     {
-        
+        _sfxVolume = volume;
+        sfxSource.volume = volume;
+        PlayerPrefs.SetFloat(SFXKEY, volume);
     }
 
     public void SetBgmVolume(float volume)
     {
-        
+        _bgmVolume = volume;
+        bgmSource.volume = volume;
+        PlayerPrefs.SetFloat(BGMKEY, volume);
     }
-
-    public float GetSfxVolume()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public float GetBgmVolume()
-    {
-        throw new System.NotImplementedException();
-    }
-
+    
     public UniTaskVoid PlayBgm(AudioClip bgm, float duration = 1)
     {
-        throw new System.NotImplementedException();
+        bgmSource.Stop();
+        bgmSource.clip = bgm;
+        bgmSource.volume = _bgmVolume;
+        bgmSource.Play();
+        return default;
     }
 
     public void PlaySfx(AudioClip sfx)
     {
-        throw new System.NotImplementedException();
+        sfxSource.PlayOneShot(sfx, _sfxVolume);
     }
 }
