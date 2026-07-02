5cc77abb97d28af759ccb53c742b6c0b199b40f5
Crowasang <bighyun620@naver.com>
2026-07-02 15:40:54 +0900

    feat: 환경설정버튼 클릭시 상호작용 못하게하는기능 추가

--- Assets/Scripts/Common/SettingUIController.cs
+++ Assets/Scripts/Common/SettingUIController.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -13,6 +14,7 @@ public class SettingUIContorller : MonoBehaviour
     [SerializeField] private Button continueBtn;
     [SerializeField] private Button soundBtn;
     [SerializeField] private Button firstBtn;
+    [SerializeField] private GameObject bgImg;
     
     [Header("Sound Panel")]
     [SerializeField] private GameObject soundPanel;
@@ -20,9 +22,14 @@ public class SettingUIContorller : MonoBehaviour
     [SerializeField] private Slider masterSlider;
     [SerializeField] private Slider sfxSlider;
     [SerializeField] private Slider bgmSlider;
+    [SerializeField] private TextMeshProUGUI masterVolume;
+    [SerializeField] private TextMeshProUGUI sfxVolume;
+    [SerializeField] private TextMeshProUGUI bgmVolume;
 
     [Header("Confirm Msg")] 
     [SerializeField] private ConfirmMsgController cmc;
+    
+    private GraphicRaycaster[] _raycasters;
     private void Start()
     {
         settingBtn.onClick.AddListener(OpenSettingPanel);
@@ -30,21 +37,42 @@ public class SettingUIContorller : MonoBehaviour
         soundBtn.onClick.AddListener(OpenSoundPanel);
         firstBtn.onClick.AddListener(GoFirst);
         closeSoundBtn.onClick.AddListener(CloseSoundPanel);
-        masterSlider.onValueChanged.AddListener(v => ServiceLocater.Get<ISoundManager>().SetMasterVolume(v));
-        bgmSlider.onValueChanged.AddListener(v => ServiceLocater.Get<ISoundManager>().SetBgmVolume(v));
-        sfxSlider.onValueChanged.AddListener(v => ServiceLocater.Get<ISoundManager>().SetSfxVolume(v));
+        masterSlider.onValueChanged.AddListener(v =>
+        {
+            ServiceLocater.Get<ISoundManager>().SetMasterVolume(v);
+            masterVolume.text = $"{(int)(v * 100)}%";
+        });
+        bgmSlider.onValueChanged.AddListener(v =>
+        {
+            ServiceLocater.Get<ISoundManager>().SetBgmVolume(v);
+            bgmVolume.text = $"{(int)(v * 100)}%";
+        });
+        sfxSlider.onValueChanged.AddListener(v =>
+        {
+            ServiceLocater.Get<ISoundManager>().SetSfxVolume(v);
+            sfxVolume.text = $"{(int)(v * 100)}%";
+        });
+        var mainRaycaster = GetComponentInParent<GraphicRaycaster>();
+        _raycasters = FindObjectsByType<GraphicRaycaster>(FindObjectsSortMode.None);
+        _raycasters = System.Array.FindAll(_raycasters, r => r != mainRaycaster);
     }
 
     private void OpenSettingPanel()
     {
         Time.timeScale = 0;
         settingPanel.SetActive(true);
+        bgImg.SetActive(true);
+        foreach (var r in _raycasters)
+            r.enabled = false;
     }
 
     private void CloseSettingPanel()
     {
         Time.timeScale = 1;
         settingPanel.SetActive(false);
+        bgImg.SetActive(false);
+        foreach (var r in _raycasters)
+            r.enabled = true;
     }
     
     private void OpenSoundPanel()
@@ -53,6 +81,9 @@ public class SettingUIContorller : MonoBehaviour
         masterSlider.value = sound.GetMasterVolume();
         sfxSlider.value = sound.GetSfxVolume();
         bgmSlider.value = sound.GetBgmVolume();
+        masterVolume.text = $"{(int)(masterSlider.value * 100)}%";
+        bgmVolume.text = $"{(int)(bgmSlider.value * 100)}%";
+        sfxVolume.text = $"{(int)(sfxSlider.value * 100)}%";
         settingPanel.SetActive(false);
         soundPanel.SetActive(true);
     }
