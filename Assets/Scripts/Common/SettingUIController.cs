b4be53ec94d10c868013b1e3a41eb75f9fb3d340
Crowasang <bighyun620@naver.com>
2026-07-02 12:57:27 +0900

    feat: SettingUIController 생성

---
+++ Assets/Scripts/Common/SettingUIController.cs
@@ -0,0 +1,59 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SettingUIContorller : MonoBehaviour
+{
+    [Header("Settings Buttons")]
+    [SerializeField] private Button settingBtn;
+    
+    [Header("Setting Panel")]
+    [SerializeField] private GameObject settingPanel;
+    [SerializeField] private Button continueBtn;
+    [SerializeField] private Button soundBtn;
+    [SerializeField] private Button firstBtn;
+    
+    [Header("Sound Panel")]
+    [SerializeField] private GameObject soundPanel;
+
+    [Header("Confirm Msg")] 
+    [SerializeField] private ConfirmMsgController cmc;
+    private void Start()
+    {
+        
+    }
+
+    private void OpenSettingPanel()
+    {
+        Time.timeScale = 0;
+        settingPanel.SetActive(true);
+    }
+
+    private void CloseSettingPanel()
+    {
+        Time.timeScale = 1;
+        settingPanel.SetActive(false);
+    }
+    
+    private void OpenSoundPanel()
+    {
+        settingPanel.SetActive(false);
+        soundPanel.SetActive(true);
+    }
+
+    private void CloseSoundPanel()
+    {
+        soundPanel.SetActive(false);
+        settingPanel.SetActive(true);
+    }
+
+    private void GoFirst()
+    {
+        cmc.Render(9900054, () =>
+        {
+            Time.timeScale = 1;
+            ServiceLocater.Get<ISceneChanger>().ChangeScene("TitleScene");
+        },null);
+    }
+}
