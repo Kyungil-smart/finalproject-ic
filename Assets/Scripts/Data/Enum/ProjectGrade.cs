112bec755d5060d222245c842980a66c270af744
156Sheong <waladeus@gmail.com>
2026-07-07 15:25:31 +0900

    fix : 프로젝트 등급 S ~ D 까지로 수정

--- Assets/Scripts/Data/Enum/ProjectGrade.cs
+++ Assets/Scripts/Data/Enum/ProjectGrade.cs
@@ -1,4 +1,10 @@
 ﻿public enum ProjectGrade
 {
-    F, E, D, C, B, A, S, SS, SSS, SSSS    
+    F = -2, // 0점 미만은 사용 안하는데, 기존 세이브 데이터와 충돌 때문에 살려놓음
+    E = -1,
+    D = 0,
+    C = 1,
+    B = 2,
+    A = 3,
+    S = 4    
 }
